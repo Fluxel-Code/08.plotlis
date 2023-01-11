f7cee50023b0896378f563ea9b29069436eea467
Fluxel-Code <110497050+Fluxel-Code@users.noreply.github.com>
2023-01-11 16:46:24 -0400

    terminada

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,17 +1,34 @@
-$('.owl-carousel').owlCarousel({
+// $('.owl-carousel').owlCarousel({
+//     loop: true,
+//     margin: 10,
+//     nav: false,
+//     dots: true,
+//     responsive: {
+//         0: {
+//             items: 1
+//         },
+//         769: {
+//             items: 1
+//         },
+//         1000: {
+//             items: 1
+//         }
+//     }
+// })
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items: 1,
     loop: true,
     margin: 10,
-    nav: true,
-    dots: false,
-    responsive: {
-        0: {
-            items: 2
-        },
-        769: {
-            items: 3
-        },
-        1000: {
-            items: 4
-        }
-    }
+    autoplay: true,
+    autoplayTimeout: 2000,
+    // autoplaySpeed: 5000,
+    autoplayHoverPause: true
+});
+$('.play').on('click', function () {
+    owl.trigger('play.owl.autoplay', [1000])
+})
+$('.stop').on('click', function () {
+    owl.trigger('stop.owl.autoplay')
 })
